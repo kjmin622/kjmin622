2333eb5c1485b24dcd66b27e373ab38ee43ef3d6
kjmin622 <kjmin622@naver.com>
2020-04-01 00:27:29 +0900

    200401

--- js/cursor.js
+++ js/cursor.js
@@ -1,12 +1,12 @@
-function resultFun(x) { 
+function Fcursor(x) { 
     var positionLeft = x.clientX; 
     var positionTop = x.clientY; 
     document.getElementById('cursor_img').style.left = positionLeft - 10 + "px"; 
     document.getElementById('cursor_img').style.top = positionTop - 10 +"px"; 
 } 
 if (document.addEventListener) { 
-    document.addEventListener("mousemove", resultFun); 
+    document.addEventListener("mousemove", Fcursor); 
 } 
 else if (document.attachEvent) { 
-    document.attachEvent("onmousemove", resultFun); 
+    document.attachEvent("onmousemove", Fcursor); 
 }
