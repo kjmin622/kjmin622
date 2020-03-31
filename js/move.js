808ed801b334d0de8377468b7ee615b05c407cd3
kjmin622 <kjmin622@naver.com>
2020-03-31 23:51:18 +0900

    200331

--- js/move.js
+++ js/move.js
@@ -1,81 +1,51 @@
-var b_up = true;
+
 var b_left = true;
 var b_right = true;
-var b_down = false;
+
 
 var now = 0; // 0 center 1 up 2 left 3 right
-var up = document.getElementById("up");
 var left = document.getElementById("left");
 var right = document.getElementById("right");
-var down = document.getElementById("down");
 
 var center = document.getElementById("profile");
-var north = document.getElementById("north");
 var east = document.getElementById("east");
 var west = document.getElementById("west");
 
 var c_left = 50;
 var c_top = 50;
-up.onclick = function(){
-    if(!b_up) return;
-    b_up=b_left=b_right=false;
-    b_down=true;
-    now=1;
-    move = setInterval(function(){
-        c_top+=1;
-        Fmove()
-        if(c_top==150){
-            center.style.visibility="hidden";
-            clearInterval(move);
-        }
-    },1);
-    visiable();
-}
-
-down.onclick = function(){
-    if(!b_down) return;
-    b_up=b_left=b_right=true;
-    b_down=false;
-    now=0;
-    move = setInterval(function(){
-        c_top-=1;
-        Fmove()
-        if(c_top==50){
-            north.style.visibility="hidden";
-            clearInterval(move);
-        }
-    },1);
-    visiable();
-}
 
 left.onclick = function(){
     if(now==0){
         b_up=b_left=b_down=false;
         b_right=true;
         now=2;
+        visiable();
+        now=-1;
         move = setInterval(function(){
             c_left+=1;
             Fmove()
             if(c_left==150){
+                now=2;
                 center.style.visibility="hidden";
                 clearInterval(move);
             }
         },1);
-        visiable();
     }
     else if(now==3){
         b_up=b_left=b_right=true;
         b_down=false;
         now=0;
+        visiable();
+        now=-1;
         move = setInterval(function(){
             c_left+=1;
             Fmove()
             if(c_left==50){
+                now=0;
                 east.style.visibility="hidden";
                 clearInterval(move);
             }
         },1);
-        visiable();
     }
 }
 
@@ -84,29 +54,34 @@ right.onclick = function(){
         b_up=b_right=b_down=false;
         b_left=true;
         now=3;
+        visiable();
+        now=-1;
         move = setInterval(function(){
             c_left-=1;
             Fmove()
             if(c_left==-50){
+                now=3;
                 center.style.visibility="hidden";
                 clearInterval(move);
             }
         },1);
-        visiable();
+        
     }
     else if(now==2){
         b_up=b_left=b_right=true;
         b_down=false;
         now=0;
+        visiable();
+        now=-1;
         move = setInterval(function(){
             c_left-=1;
             Fmove()
             if(c_left==50){
+                now=0;
                 west.style.visibility="hidden";
                 clearInterval(move);
             }
         },1);
-        visiable();
     }
 }
 
@@ -117,15 +92,9 @@ function Fmove(){
     west.style.top = c_top+'%';
     east.style.left = 100+c_left+'%';
     east.style.top = c_top+'%';
-    north.style.left = c_left+'%';
-    north.style.top = -100+c_top+'%';
 }
 
 function visiable(){
-    if(b_up) up.style.visibility = "visible";
-    else up.style.visibility = "hidden";
-    if(b_down) down.style.visibility = "visible";
-    else down.style.visibility = "hidden";
     if(b_left) left.style.visibility = "visible";
     else left.style.visibility = "hidden";
     if(b_right) right.style.visibility = "visible";
@@ -133,18 +102,11 @@ function visiable(){
 
     
     center.style.visibility = "visible";
-    if(now==1){
-        north.style.visibility = "visible";
-        west.style.visibility = "hidden";
-        east.style.visibility = "hidden";
-    }
     if(now==2){
-        north.style.visibility = "hidden";
         west.style.visibility = "visible";
         east.style.visibility = "hidden";
     }
     if(now==3){
-        north.style.visibility = "hidden";
         west.style.visibility = "hidden";
         east.style.visibility = "visible";
     }
